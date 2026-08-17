9c43dece50de99d8dab9f0711a2bc9466e8830ce
konyakpivo-wq <konyakpivo@yandex.com>
2026-08-17 17:16:08 +0200

    Port Bun Mod item to Minecraft 1.13.2 food API

--- src/main/java/ru/konyakpivo/bunmod/BunMod.java
+++ src/main/java/ru/konyakpivo/bunmod/BunMod.java
@@ -1,8 +1,8 @@
 package ru.konyakpivo.bunmod;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
@@ -10,21 +10,26 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = BunMod.MODID, name = BunMod.NAME, version = BunMod.VERSION, acceptedMinecraftVersions = "[1.12.2]")
+@Mod(modid = BunMod.MODID, name = BunMod.NAME, version = BunMod.VERSION, acceptedMinecraftVersions = "[1.13.2]")
 @Mod.EventBusSubscriber
 public class BunMod {
     public static final String MODID = "bunmod";
     public static final String NAME = "Bun Mod";
     public static final String VERSION = "1.0.0";
 
+    private static final Food BUN_FOOD = new Food.Builder()
+            .hunger(1)
+            .saturation(0.1F)
+            .build();
+
     public static Item BUN;
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        BUN = new ItemFood(1, 0.1F, false)
-                .setUnlocalizedName(MODID + ".bun")
-                .setRegistryName(new ResourceLocation(MODID, "bun"))
-                .setCreativeTab(CreativeTabs.FOOD);
+        BUN = new Item(new Item.Properties()
+                .food(BUN_FOOD)
+                .group(CreativeTabs.FOOD))
+                .setRegistryName(new ResourceLocation(MODID, "bun"));
         event.getRegistry().register(BUN);
     }
 
